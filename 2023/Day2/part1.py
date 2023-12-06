31cd09cd40fa6cefe67404ebd6c392c642a88b00
Cole Ramey <ceramey1997@gmail.com>
2023-12-05 19:05:22 -0600

    Update part1 works for day2

--- 2023/Day2/part1.py
+++ 2023/Day2/part1.py
@@ -1,8 +1,12 @@
 from typing import List
 
 class Color:
-    color: str
-    amount: int
+    def __init__(self):
+        self.color : str
+        self.amount : int
+
+    def __str__(self):
+        return f"{self.amount} {self.color}"
 
     def parse(self, partial_line : str):
         partial_line = partial_line.strip()
@@ -11,7 +15,17 @@ class Color:
         self.color = color.strip()
 
 class Pull:
-    colors: List[Color]
+
+    def __init__(self):
+        self.colors: List[Color]
+
+    def __str__(self):
+        ret = ""
+        for i in range(len(self.colors)):
+            ret += f"{self.colors[i]}"
+            if not i == len(self.colors) - 1:
+                ret += ", "
+        return ret
 
     def parse(self, partial_line: str):
         split_colors = partial_line.split(",")
@@ -21,111 +35,96 @@ class Pull:
             single_color.parse(c)
             self.colors.append(single_color)
 
-class Series:
-    pulls: List[Pull]
+class Game:
+    def __init__(self):
+        self.num:int
+        self.pulls : List[Pull] = []
 
-    def parse(self, partial_line: str):
-        split_pulls = partial_line.split(";")
-        self.pulls = []
+    def __str__(self):
+        ret = f"Game {self.num}: "
+        for i in range(len(self.pulls)):
+            ret += f"{self.pulls[i]}"
+            if not i == len(self.pulls) - 1:
+                ret += "; "
+        return ret
+
+    def parse(self, line: str):
+        game, pulls_unparsed = line.split(":")
+        self.num = int(game.split(" ")[1])
+        split_pulls = pulls_unparsed.split(';')
         for p in split_pulls:
             single_pull = Pull()
             single_pull.parse(p.strip())
             self.pulls.append(single_pull)
 
-class Game:
-    num:int
-    series : Series
-    def parse(self, line: str):
-        game, series_unparsed = line.split(":")
-        self.num = int(game.split(" ")[1])
-        self.series = Series()
-        self.series.parse(series_unparsed)
+class FindItAll:
 
-bag = {
-    "red": 12,
-    "green": 13,
-    "blue": 14
-}
+    def __str__(self):
+        #for game in self._impossibles:
+        #    print(game)
+        #for game in self._possibles:
+        #    print(game)
+        pos_ints = [g.num for g in self._possibles]
+        #impos_ints = [g.num for g in self._impossibles]
+        #print(f"Impossibles\n {impos_ints}")
+        #print(f"Possibles\n {pos_ints}")
 
-file = open('input1.txt', 'r')
-games :List[Game] = []
-while True:
-    line = file.readline()
-    if not line:
-        break
-    game = Game()
-    game.parse(line)
-    games.append(game)
+        tot = 0
+        for g in pos_ints:
+            tot = tot + g
+        return str(tot)
 
-import pdb;
-totals = {
-    "red": 0,
-    "green": 0,
-    "blue": 0
-}
 
-impossibles : list[int] = []
-possibles: list[int] = []
+    def __init__(self):
+        self._games: List[Game] = []
+        self._impossibles : List[Game] =[] 
+        self._possibles: List[Game] = []
+        self._bag = {
+            "red": 12,
+            "green": 13,
+            "blue": 14
+        }
+        file = open('input1.txt', 'r')
+        while True:
+            line = file.readline()
+            if not line:
+                break
+            game = Game()
+            game.parse(line)
+            self._games.append(game)
+        for g in self._games:
+            possible = self.is_game_possible(g)
+            if possible:
+                self._possibles.append(g)
+                continue
+            self._impossibles.append(g)
 
-print_game_possibles :List[str]= []
 
-for g in games:
-    game_print = f"{g.num}: "
-    for p in g.series.pulls:
-        for c in p.colors:
-            totals[c.color]+= c.amount
-            game_print += f"{c.amount} {c.color}, "
-        game_print +="; "
-        pdb.set_trace()
-    if totals["red"] > bag["red"]:
-        impossibles.append(g.num)
 
+    def is_game_possible(self, game: Game) -> bool:
+        for pull in game.pulls:
+            possible = self.pull_possible(pull)
+            if not possible:
+                return False
+        return True
+
+    def pull_possible(self, pull: Pull) -> bool:
         totals = {
             "red": 0,
             "green": 0,
             "blue": 0
         }
-        #print(f"{g.num} -> false b/c red")
-        #print(totals["red"])
-        game_print = ""
-        continue
-    if totals["green"] > bag["green"]:
-        impossibles.append(g.num)
-        #print(f"{g.num} -> false b/c green")
-        totals = {
-            "red": 0,
-            "green": 0,
-            "blue": 0
-        }
-        game_print = ""
-        continue
-    if totals["blue"] > bag["blue"]:
-        impossibles.append(g.num)
-        totals = {
-            "red": 0,
-            "green": 0,
-            "blue": 0
-        }
-        #print(f"{g.num} -> false b/c blue")
-        game_print = ""
-        continue
+        for c in pull.colors:
+            totals[c.color] += c.amount
 
-    print_game_possibles.append(game_print)
-    game_print = ""
-    possibles.append(g.num)
-    #print(totals)
-    totals = {
-        "red": 0,
-        "green": 0,
-        "blue": 0
-    }
-    #print(totals)
+        if totals["red"] > self._bag["red"]:
+            return False
+        if totals["green"] > self._bag["green"]:
+            return False
+        if totals["blue"] > self._bag["blue"]:
+            return False
+        return True
 
-print(possibles)
-for pp in print_game_possibles:
-    print(pp)
-totals = 0
-for p in possibles:
-    totals += p
-print(impossibles)
-print(totals)
+
+g = FindItAll()
+print(g)
