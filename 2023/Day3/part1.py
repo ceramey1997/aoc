a10afc105fc92b8ba2fd3040438433f9140bd923
Cole Ramey <ceramey1997@gmail.com>
2023-12-08 21:30:55 -0600

    closer

--- 2023/Day3/part1.py
+++ 2023/Day3/part1.py
@@ -2,13 +2,12 @@
 class Schematic:
     def __str__(self):
         gg = ""
-        for i in range(len(self._rows)):
-            gg += f"{self._rows[i]}\n"
+        #for i in range(len(self._rows)):
+        #    gg += f"{self._rows[i]}\n"
         total = 0
         for j in range(len(self._part_numbers)):
             total += self._part_numbers[j]
-            gg += f"{self._part_numbers[j]}\n"
-
+            #gg += f"{self._part_numbers[j]}\n"
         gg += f"****************{total}************"
         return gg
 
@@ -32,7 +31,7 @@ class Schematic:
         is_bottom = False
         if row_above < 0:
             is_top = True
-        if row_below > len(self._rows):
+        if row_below >= len(self._rows):
             is_bottom = True 
 
         is_beginning = False
@@ -40,18 +39,24 @@ class Schematic:
         col_number = 0
         while col_number < len(self._rows[row_number]):
             start = col_number
-            if (not self.is_int(self._rows[row_number][col_number])):
+            has_ints = False
+            while True:
+                try:
+                    is_an_int = self.is_int(self._rows[row_number][col_number])
+                    if not is_an_int:
+                        break
+                    has_ints = True
+                    col_number += 1
+                except Exception:
+                    break
+
+            if not has_ints:
                 col_number += 1
                 continue
-            col_number += 1
-            while (self.is_int(self._rows[row_number][col_number])):
-                col_number += 1
+
             if start == 0:
                 is_beginning = True
 
-            if row_number == 7 and start == 6 and col_number-1 == 8:
-                #import pdb; pdb.set_trace()
-                print()
             if col_number == len(self._rows[row_number]):
                 is_end = True
 
@@ -60,13 +65,11 @@ class Schematic:
                 part_number = ""
                 j = start
                 while j < col_number:
-                    print(self._rows[row_number][j], end="")
+                    #print(self._rows[row_number][j], end="")
                     part_number += self._rows[row_number][j]
                     j += 1
                 self._part_numbers.append(int(part_number))
                 part_number = ""
-                print()
-                print("hi")
             col_number += 1
 
     def is_int(self, char) -> bool:
@@ -77,9 +80,6 @@ class Schematic:
             return False
 
     def has_symbol_around(self, row_number: int, col_num_start: int, col_num_end: int, is_top: bool, is_bottom: bool, is_beginning: bool, is_end: bool) -> bool:
-        if row_number == 7 and col_num_start == 6 and col_num_end == 8:
-            #import pdb; pdb.set_trace()
-            print()
         if not is_beginning:
             if self._rows[row_number][col_num_start - 1] != '.' and not self.is_int(self._rows[row_number][col_num_start - 1]):
                 return True
@@ -87,24 +87,28 @@ class Schematic:
             if self._rows[row_number][col_num_end + 1] != '.' and not self.is_int(self._rows[row_number][col_num_end + 1]):
                 return True
         if not is_top:
-            j = col_num_start - 1
+            j = col_num_start
+            if not is_beginning:
+                j = col_num_start - 1
             while j <= col_num_end + 1:
                 if j == col_num_start and is_beginning:
                     j += 1
                     continue
-                if j == col_num_end and is_end:
+                if j >= col_num_end and is_end:
                     j += 1
                     continue
                 if self._rows[row_number - 1][j] != '.' and not self.is_int(self._rows[row_number - 1][j]):
                     return True
                 j += 1
         if not is_bottom:
-            k = col_num_start - 1
+            k = col_num_start
+            if not is_beginning:
+                k = col_num_start - 1
             while k <= col_num_end + 1:
                 if k == col_num_start and is_beginning:
                     k += 1
                     continue
-                if k == col_num_end and is_end:
+                if k >= col_num_end and is_end:
                     k += 1
                     continue
                 if self._rows[row_number + 1][k] != '.' and not self.is_int(self._rows[row_number + 1][k]):
